c1a7d348e7432cea1144bf258e8463cd20d56639
1224847912 <1224847912@qq.com>
2020-03-14 17:37:45 +0800

    初始化项目以及底部导航基本设置

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
+    <tab-bar>
+      <tab-bar-item path="/home">
+        <img slot="item-img" src="~assets/img/tabbar/home.png" alt="">
+        <img slot="item-img-active" src="~assets/img/tabbar/home-active.png" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category">
+        <img slot="item-img" src="~assets/img/tabbar/category.png" alt="">
+        <img slot="item-img-active" src="~assets/img/tabbar/categroy-active.png" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart">
+        <img slot="item-img" src="~assets/img/tabbar/car.png" alt="">
+        <img slot="item-img-active" src="~assets/img/tabbar/car-active.png" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <img slot="item-img" src="~assets/img/tabbar/profile.png" alt="">
+        <img slot="item-img-active" src="~assets/img/tabbar/profile-active.png" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  import TabBar from 'components/common/tabbar/Tabbar'
+  import TabBarItem from 'components/common/tabbar/TabbarItem'
+  export default {
+    name: 'app',
+    components: {
+      TabBar,
+      TabBarItem
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/common/tabbar/TabbarItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-img"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-img-active"></slot>
+    </div>
+    <div :class="{active:isActive}">
+      <slot name="item-text"></slot>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    data() {
+      return {
+        isActive: true
+      }
+    },
+    props: {
+      path: String
+    },
+    methods: {
+      itemClick() {
+        this.$router.replace(this.path)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    height: 49px;
+    text-align: center;
+    font-size: 14px;
+    margin-top: 3px;
+  }
+
+  .tab-bar-item img {
+    height: 24px;
+    width: 24px;
+  }
+
+  .active {
+    color: red;
+  }
+</style>
